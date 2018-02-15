9fefaccf40ad92c77464ad5ce81baf712919a770
Neous1 <Neous1@users.noreply.github.com>
2018-02-15 14:18:58 -0500

    initial commit

---
+++ SimpleArraySum/SimpleArraySum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = simpleArraySum(n, ar);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+
+        static int simpleArraySum(int n, int[] ar)
+        {
+            // Complete this function
+
+            return 0;
+        }
+
+        
+    }
+}
